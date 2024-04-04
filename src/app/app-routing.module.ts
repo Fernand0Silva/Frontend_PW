5c0e77691ee4725128901d1b8af506d9b82b8e23
luiz <luizfernando3995@gmail.com>
2024-04-04 19:19:15 -0300

    atualizacao do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListaProdutosComponent } from './components/produtos/lista-produtos/lista-produtos.component';
+import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'produtos', component:ListaProdutosComponent},
+  {path:'produtos/cadastrar', component:CadastrarProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
