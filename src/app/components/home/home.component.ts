7e84dff5e8f0439a446264578cc950e489cd23b2
luiz <luizfernanado3995@gmail.com>
2024-04-11 21:25:21 -0300

    exercicios de variaveis

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent {
       //Variaveis de string com concatenação
       //this.anuncio = `O` + this.nomeProduto + `está em promoção`;
 
-      console.log(`Nome do Produto: `, this.nomeProduto);
+      /*console.log(`Nome do Produto: `, this.nomeProduto);
       console.log(`Anuncio `, this.anuncio);
       console.log(`ID `, this.idProduto);
       console.log(`Preço: `, this.precoProduto);
@@ -51,7 +51,7 @@ export class HomeComponent {
           console.log(`Minha idade é: `, idade);
     }
       imprimeIdade() */
-      function imprimeIdade()
+      /*function imprimeIdade()
       {
         for (let idade = 30; idade <= 40; idade++)
         {
@@ -59,12 +59,69 @@ export class HomeComponent {
         }
         console.log(`Idade fora do for: `, idade);
       }
-      imprimeIdade();
+      imprimeIdade(); */
 
+      /*let variavel = 0;
+      let variavel = 10;
+      console.log('variavel', variavel);
+      function funcao()
+      {
+        let outra_variavel = 20;
+        let outra_variavel = 30;
+        console.log('outra variavel:',outra_variavel); // variavel apresenta erro porque: let pode ser atualizado, mas não declarado novamente.
+      }
+      funcao();
 
+      /*var a = 10;
+      let b = 20;
+      function bar()
+      {
+        var a = 30;
+        let b = 40;
+        if(true)
+        {
+          var a = 50;
+          let b = 60;
+          console.log('Variável a dentro do if', a);
+          console.log('Variável b dentro do if', b);
+        }
+        console.log('Variável a dentro da função', a);
+        console.log('Variável b dentro da função', b);
+      }
+      bar();
+      console.log('Variável a fora da função', a);
+      console.log('Variável b fora da função', b); */
 
+      /*let a = 1;
+      function bar()
+      {
+        console.log('variável a dentro do if: ', a);
+        let b = 2;
+        if(true)
+        {
+          let c = 3;
+          let a = 4;
+          console.log('variável a dentro do if: ', a);
+          console.log('variável b dentro do if: ', b);
+          console.log('variável c dentro do if: ', c);
+        }
+        console.log('variável a dentro do if: ', a);
+        console.log('variável b dentro do if: ', b);
+        //console.log(c);
+      }
+      bar(); */
 
-
+      /*var a = 5;
+      var b = 10;
+      if(a = 5)
+      {
+        let a = 4;
+        var b = 1;
+        console.log('Variável a dentro do if: ', a);
+        console.log('Variável b dentro do if: ', b);
+      }
+      console.log('Variável a fora do if: ', a);
+      console.log('Variável b fora do if: ', b); */
 
      }
 
