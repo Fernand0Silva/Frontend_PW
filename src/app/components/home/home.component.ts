89d51c748f5335a4d7d920003e31c1c07ffc7317
luiz <luizfernando3995@gmail.com>
2024-05-02 22:21:13 -0300

    shared

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app- card-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
